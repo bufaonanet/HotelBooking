16d40497de3a98b2948f3f5f8722ab4be150b9db
bufaonanet <douglas.bufaonanet@outlook.com>
2022-10-31 11:18:39 -0300

    Add project files.

---
+++ BookingService/Core/Domain/Entities/Room.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.Entities;
+
+public class Room
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Level { get; set; }
+    public bool InMaintenence { get; set; }
+    public bool IsAvailabel
+    {
+        get
+        {
+            if (InMaintenence || HasGest)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public bool HasGest
+    {
+        //Verificar se existem bookins abertos para este room
+        get { return true; }
+    }
+}
---
+++ BookingService/Tests/Adapters/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Adapters
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
